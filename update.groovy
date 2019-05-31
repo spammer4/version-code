77b92f107e9c05fc8077ab95a7521c9bcd94669b
paul <spammer4@planetsugar.co.uk>
2019-05-31 12:10:33 +0100

    v1.5.0-rc

--- update.groovy
+++ update.groovy
@@ -9,14 +9,20 @@ def sh(String command) {
     return "$serr $sout".trim()
 }
 
+def getVersionFromPackageJson() {
+    String awkCommand = 'awk -F\'"\' \'/"version": ".+"/{ print $4; exit; }\' package.json'
+    return sh(awkCommand)
+}
+
 def updatePackageVersion(String version, Boolean isDevelop = true) {
+    def String RELEASE_CLIENT_EXTENSION = "-rc"
     if (isDevelop) {
-        if (version.contains("-release")) {
+        if (version.contains(RELEASE_CLIENT_EXTENSION)) {
             println "Remove the release and commit"
-            String newVersion = version.replace("-release","")
+            String newVersion = version.replace(RELEASE_CLIENT_EXTENSION,"")
             sh "yarn.cmd version --no-git-tag-version --new-version ${newVersion}"
             sh "git add ."
-            sh "git commit -m 'Removed -release from develop version ${version}'"
+            sh "git commit -m 'Removed ${RELEASE_CLIENT_EXTENSION} from develop version ${version}'"
             sh "git push"
         }
         
@@ -24,17 +30,25 @@ def updatePackageVersion(String version, Boolean isDevelop = true) {
     } 
 
     // Release 
+    if (!version.contains(RELEASE_CLIENT_EXTENSION)) {
+        // Initial build of the release branch from develop
+        println "Initial release cut, incrementing minor version number"
+        sh "yarn.cmd version --no-git-tag-version --new-version minor"
+    } else {
+        // Hotfix, need to patch twice, the first removes the -release and the second 
+        // increments the patch number 
+        println "Hotfix applied, incrementing patch number"
+        sh "yarn.cmd version --no-git-tag-version --new-version patch"
+        sh "yarn.cmd version --no-git-tag-version --new-version patch"
+    }
 
-    return
-}
-
-def getVersionFromPackageJson() {
-    String awkCommand = 'awk -F\'"\' \'/"version": ".+"/{ print $4; exit; }\' package.json'
-    return sh(awkCommand)
+    // Get the updated version, append -release to it and tag it 
+    String updatedVersion = getVersionFromPackageJson()
+    sh "yarn.cmd version --new-version ${updatedVersion}${RELEASE_CLIENT_EXTENSION}"
 }
 
 String currentVersion = getVersionFromPackageJson()
-updatePackageVersion(currentVersion)
+updatePackageVersion(currentVersion, true)
 /*def getUpdatedVersion(String current) {
         
 }*/
